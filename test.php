00292ba4e9b53ea4e437a3db9f1235c14af49c40
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 15:13:51 +0500

    new file added 0313

--- test.php
+++ test.php
@@ -7,12 +7,12 @@ require "database/config.php";
     $dbname = "mysampledata";
 
 ?>
-<h1> 0258  </h1>
+<h1> 0313  </h1>
 
 <?php 
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, 'satserver01.mysql.database.azure.com', 'azureusergeneral', 'NGli5ESz@eq1b8RzsE', 'mysampledata', 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno()) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
