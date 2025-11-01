f6bb827ef882edfc33ce97e206974d82894702cf
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 14:10:41 +0500

    new file added 04

--- read.php
+++ read.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// Your PHP code follows here
+?>
 <?php require "templates/header.php"; ?>
 
 <div class="center-align">
