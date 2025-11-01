ceab1624dde446961a5660250caa4f188249c62f
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 14:58:47 +0500

    new file added 0258

--- test.php
+++ test.php
@@ -1,46 +1,21 @@
 <?php 
 require "database/config.php";
-?>
-<h1> One  </h1>
-<?php
+
     $servername = "satserver01.mysql.database.azure.com";
     $username = "azureusergeneral";
     $password = "NGli5ESz@eq1b8RzsE";
     $dbname = "mysampledata";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
 ?>
-<br/ ><br/ ><br/ ><br/ ><br/ >
-<h1> Seocnd </h1>
-<?php
-    $host = "satserver01.mysql.database.azure.com";
-    $username = "azureusergeneral";
-    $password = "NGli5ESz@eq1b8RzsE";
-    $database = "mysampledata"; 
-    $sslcert = "DigiCertGlobalRootCA.pem";
+<h1> 0258  </h1>
 
-    // Initialize MySQLi
-    $conn = mysqli_init();
+<?php 
 
-    // Set SSL options
-    mysqli_ssl_set($conn, NULL, NULL, $sslcert, NULL, NULL);
-
-    // Establish the connection
-    if (!mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL)) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-
-    echo "Connected successfully to Azure Database for MySQL Flexible Server!";
-
-    // Perform database operations here (e.g., create tables, insert, read, update, delete)
-
-    // Close the connection
-    mysqli_close($conn);
-    ?>
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, 'mydemoserver.mysql.database.azure.com', username: $username, password: $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno()) {
+  die('Failed to connect to MySQL: '.mysqli_connect_error());
+}else{
+  echo "Connection Works fine";
+}
