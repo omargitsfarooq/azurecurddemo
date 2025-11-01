a5e49642b177a19e08cd182dbf6cb97d2ad35664
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 15:55:41 +0500

    new file added 0355

--- database/config.php
+++ database/config.php
@@ -7,5 +7,5 @@ error_reporting(E_ALL);
 $host       = getenv('DB_HOST');
 $username   = getenv('DB_USERNAME');
 $password   = getenv('DB_PASSWORD');
-$db_name     = getenv('DB_DATABASE');
+$db_name    = getenv('DB_DATABASE');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
--- test.php
+++ test.php
@@ -1,16 +1,49 @@
-<h1>340</h1>
+<h1> 0355 </h1>
 <?php
 
-    require "database/config.php";
+/**
+ * @file
+ * Example: Secure MySQL connection using SSL (Azure MySQL Flexible Server).
+ */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    //Establish the connection
-    $conn = mysqli_init();
-    mysqli_ssl_set($conn,NULL,certificate: NULL,ca_certificate: $sslcert,NULL,cipher_algos: NULL);
+$host = "satserver01.mysql.database.azure.com";
+$username = "azureusergeneral";
+$password = "NGli5ESz@eq1b8RzsE";
+$database = "mysampledata";
+$port = 3306;
 
+// Initialize connection.
+$conn = mysqli_init();
 
-    if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
-    }else{
-      echo "Connect";
-    }
+// Set SSL parameters (adjust path if needed).
+mysqli_ssl_set(
+  $conn,
+  NULL,
+  NULL,
+  '/DigiCertGlobalRootCA.crt.pem',
+  NULL,
+  NULL
+);
+
+// Establish secure connection.
+mysqli_real_connect(
+  $conn,
+  $host,
+  $username,
+  $password,
+  $database,
+  $port,
+  NULL,
+  MYSQLI_CLIENT_SSL
+);
+
+// Check for connection errors.
+if (mysqli_connect_errno($conn)) {
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+print "Connection established.\n";
