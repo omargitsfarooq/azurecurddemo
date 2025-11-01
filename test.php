4050482ba220945395397fe669acd9292999cb58
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 15:40:28 +0500

    new file added 0340

--- test.php
+++ test.php
@@ -1,19 +1,16 @@
-<?php 
-require "database/config.php";
+<h1>340</h1>
+<?php
 
-    $servername = "satserver01.mysql.database.azure.com";
-    $username = "azureusergeneral";
-    $password = "NGli5ESz@eq1b8RzsE";
-    $dbname = "mysampledata";
+    require "database/config.php";
 
-?>
-<h1> 0313  </h1>
 
-<?php 
+    //Establish the connection
+    $conn = mysqli_init();
+    mysqli_ssl_set($conn,NULL,certificate: NULL,ca_certificate: $sslcert,NULL,cipher_algos: NULL);
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'satserver01.mysql.database.azure.com', 'azureusergeneral', 'NGli5ESz@eq1b8RzsE', 'mysampledata', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno()) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+
+    if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+        die('Failed to connect to MySQL: '.mysqli_connect_error());
+    }else{
+      echo "Connect";
+    }
