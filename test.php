25ef005e4db3f768d26fe93a2fd82c510e196102
Muhammad Omar Farooq <ahaomar@yahoo.com>
2025-11-01 14:40:50 +0500

    new file added 06

--- test.php
+++ test.php
@@ -1,3 +1,4 @@
+<h1> One  </h1>
 <?php
     $servername = "satserver01.mysql.database.azure.com";
     $username = "azureusergeneral";
@@ -12,4 +13,31 @@
         die("Connection failed: " . $conn->connect_error);
     }
     echo "Connected successfully";
+?>
+<br/ ><br/ ><br/ ><br/ ><br/ >
+<h1> Seocnd </h1>
+<?php
+    $host = "satserver01.mysql.database.azure.com";
+    $username = "azureusergeneral";
+    $password = "NGli5ESz@eq1b8RzsE";
+    $database = "mysampledata"; 
+    $sslcert = "DigiCertGlobalRootCA.pem";
+
+    // Initialize MySQLi
+    $conn = mysqli_init();
+
+    // Set SSL options
+    mysqli_ssl_set($conn, NULL, NULL, $sslcert, NULL, NULL);
+
+    // Establish the connection
+    if (!mysqli_real_connect($conn, $host, $username, $password, $database, 3306, MYSQLI_CLIENT_SSL)) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    echo "Connected successfully to Azure Database for MySQL Flexible Server!";
+
+    // Perform database operations here (e.g., create tables, insert, read, update, delete)
+
+    // Close the connection
+    mysqli_close($conn);
     ?>
